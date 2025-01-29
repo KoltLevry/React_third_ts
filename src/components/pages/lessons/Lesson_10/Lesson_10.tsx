b5db4234fdd22af7aac9db3b924d9417a292d885
Tanya <kolt.levry@gmail.com>
2025-01-29 13:22:11 +0100

    start axios

--- src/components/pages/lessons/Lesson_10/Lesson_10.tsx
+++ src/components/pages/lessons/Lesson_10/Lesson_10.tsx
@@ -2,28 +2,54 @@ import { Lesson10Wrapper, ButtonWrapper, ResultBlock, TitleFirst, JokeImage } fr
 
 import jokeImg from "../../../../assets/jokes/joke-1.png";
 import Button from './../../../Button/Button';
+import Spinner from "../../../Spinner/Spinner";
 import { useState } from "react";
+import axios from "axios";
 
 function Lesson10() {
-
-    const [joke, setJoke] = useState<string>("");
     
+    const [joke, setJoke] = useState<string>("");
+    const [error, setError] = useState<undefined | string>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    //  fetch
+    
+    // const fetchJokeData = async () => {
+    //     const JOKE_URL: string = 'https://official-joke-api.appspot.com/jokes/random';
+
+    //     try {
+    //         const response = await fetch(JOKE_URL);    
+    //         const result = await response.json();   
+
+    //         if(response.ok) {
+    //             setJoke(`${result.setup} - ${result.punchline}`)
+    //         } else {
+    //             throw new Error("API Error: -_-")
+    //         }
+    //     } catch(error)  {
+    //         console.log(error.messsage);
+    //     } finally {
+    //         console.log("End")
+    //     }
+
+    // }
+
+    // axios
+
     const fetchJokeData = async () => {
         const JOKE_URL: string = 'https://official-joke-api.appspot.com/jokes/random';
+        setJoke("");
+        setError(undefined);
 
         try {
-            const response = await fetch(JOKE_URL);    
-            const result = await response.json();   
-
-            if(response.ok) {
-                setJoke(`${result.setup} - ${result.punchline}`)
-            } else {
-                throw new Error("API Error: -_-")
-            }
-        } catch(error)  {
-            console.log(error.messsage);
+            setIsLoading(true);
+            const result = await axios(JOKE_URL);  
+            const data = result.data;
+            setJoke(`${data.setup} - ${data.punchline}`)
+        } catch(error: any)  {
+            setError(error.message)
         } finally {
-            console.log("End")
+            setIsLoading(false);
         }
 
     }
@@ -34,7 +60,9 @@ function Lesson10() {
             <ButtonWrapper>
                 <Button name="Click me to get JOKE!" onClick={fetchJokeData}/>
             </ButtonWrapper>
-            <ResultBlock>Text: {joke}</ResultBlock>
+            {/* <ResultBlock>Text: {joke}</ResultBlock> */}
+            {joke &&  <ResultBlock>{joke}</ResultBlock>}
+            {isLoading && <Spinner/>}
         </Lesson10Wrapper>
         <Lesson10Wrapper>
             <TitleFirst>Funny jokes!</TitleFirst>
