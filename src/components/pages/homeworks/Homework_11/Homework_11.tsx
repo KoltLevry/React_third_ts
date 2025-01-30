342524598430a1de6ea5e0648d295e76a83fda9b
Tanya Maxar <kolt.levry@gmail.com>
2025-01-30 13:09:35 +0100

    add all homework 11

--- src/components/pages/homeworks/Homework_11/Homework_11.tsx
+++ src/components/pages/homeworks/Homework_11/Homework_11.tsx
@@ -1,45 +1,79 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { 
+        HederBox,
         HwWrapper, 
         GreetingText,
         CardsContainer,
         CatCard,
+        ErrorBlock,
     } from "./styles.ts";
-// import Input from './../../../Input/Input';
-// import axios from "axios";
-// import Spinner from './../../../Spinner/Spinner';
-// import Button from "../../../Button/Button.tsx";
+import Spinner from './../../../Spinner/Spinner';
+import Button from "../../../Button/Button.tsx";
 
 function Homework_11() {
 
-  
+    const [catFacts, setCatFacts] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<undefined | string>(undefined);
+
+    const getCatFact = async () => {
+        const CAT_FACT_URL: string = "https://catfact.ninja/fact";
+        setError(undefined);
+        setIsLoading(true);
+
+        try {
+            const result = await axios.get(CAT_FACT_URL);
+            // setCatFacts(result.data.fact);
+            setCatFacts( (prevFacts) => [...prevFacts, result.data.fact]);
+        } catch(error: unknown) {
+            if(error instanceof Error) {
+                setError(error.message);
+            } else {
+                setError("Unknown error!")
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    const deleteAllFacts = () => {
+        setCatFacts([]);
+    }
+
+    useEffect( () => {
+        getCatFact();
+    }, [])
    
     return (
       <>
           <HwWrapper>
-           <GreetingText>Cats Facts</GreetingText>
-           <CardsContainer>
-                <CatCard>
-                    <p>Text</p>
-                    <p>Image</p>
-                </CatCard>
-                <CatCard>
-                    <p>Text 1</p>
-                    <p>Image 1</p>
-                </CatCard>
-                <CatCard>
-                    <p>Text 1</p>
-                    <p>Image 1</p>
-                </CatCard>
-                <CatCard>
-                    <p>Text 1</p>
-                    <p>Image 1</p>
-                </CatCard>
-                <CatCard>
-                    <p>Text 1</p>
-                    <p>Image 1</p>
-                </CatCard>
-           </CardsContainer>
+
+           <HederBox>
+                <Button 
+                    name="DELETE ALL DATA"
+                    onClick={deleteAllFacts}
+                    disabled={catFacts.length === 0}
+                ></Button>
+                <GreetingText>Cats Facts</GreetingText>
+                <Button 
+                    name="GET MORE INFO"
+                    onClick={getCatFact}
+                ></Button>
+
+            </HederBox>
+          
+            { catFacts.length > 0 && (
+                <CardsContainer>
+                    { catFacts.map((fact, index) => (
+                        <CatCard key={index}>{fact}</CatCard>
+                    ))}           
+                </CardsContainer>
+            )}
+
+            {isLoading &&  <Spinner></Spinner>}
+            {error && <ErrorBlock>{error}</ErrorBlock>}        
+
           </HwWrapper>
       </>
     );
--- src/components/pages/homeworks/Homework_11/styles.ts
+++ src/components/pages/homeworks/Homework_11/styles.ts
@@ -1,43 +1,51 @@
 import styled from "@emotion/styled";
 
+export const HederBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  min-width: 100%;
+`;
+
 export const HwWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 40px;
-  /* width: 300px; */
   min-height: 100%;
   min-width: 100%;
 `;
 
-export const GreetingText = styled.h1`
-background-color: red;
-`
+export const GreetingText = styled.h2`
+  color: red;
+  text-shadow: 2px 4px 6px rgba(0, 0, 0, 0.4);
+`;
 
 export const CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 20px;
   min-width: 100%;
-`
+
+  @media (max-width: 1810px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+`;
 
 export const CatCard = styled.div`
-// ! max-width ? 
-// todo max-width ? 
-// ~ max-width ? 
-// // max-width ? 
-width: 700px;
-padding: 30px;
-border: 2px solid darkblue;
-border-radius: 10px;
-box-shadow: 0 4px 8px rgba(0,0,0,0.2);
-background-color: #fff;
-`
-
-export const PhotoContainer = styled.img`
-  width: 300px;
-  height: 300px;
-  margin-top: 10px;
+  width: 700px;
+  height: 130px;
+  padding: 30px;
+  border: 2px solid darkblue;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  overflow-y: scroll;
 `;
 
 export const ErrorBlock = styled.div`
@@ -50,47 +58,3 @@ export const ErrorBlock = styled.div`
   font-size: 28px;
   font-weight: bold;
 `;
-
-export const ResultContainer = styled.div`
-width: 300px;
-height: 300px;
-`
-
-export const SpinnerContainer = styled.div`
-display: flex;
-justify-content: center;
-margin: 10px;
-`
-// new card
-
-export const SimpleText = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;`
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const ResultBlock = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
- font-size: 24px;
-  min-height: 300px;
-  padding: 20px;
-  font-weight: bold;
-  font-size: 30px;
-  border-radius: 16px;
-`;
-
-export const ResultBlockImg = styled.img`
-
-width: 300px;
-  height: 300px;
-`;
-
-
