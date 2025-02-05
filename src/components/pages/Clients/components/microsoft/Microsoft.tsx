8a97832e5f596f9bdf2b8d4ddc62f6fd6b22440c
Tanya <kolt.levry@gmail.com>
2025-02-05 09:47:04 +0100

    add hm 13 again

---
+++ src/components/pages/Clients/components/microsoft/Microsoft.tsx
@@ -0,0 +1,10 @@
+import {PageTitle} from './styles';
+
+function Microsoft() {
+    return (
+    <PageTitle>Welcome to the Facebook company page</PageTitle>
+    )
+  }
+  
+  export default Microsoft;
+  
