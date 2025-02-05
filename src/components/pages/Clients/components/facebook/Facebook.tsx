7e04233ad119dc32d4875ab62881b5413db90f04
Tanya <kolt.levry@gmail.com>
2025-02-05 09:40:44 +0100

    start hm 13

--- src/components/pages/Clients/components/facebook/Facebook.tsx
+++
@@ -1,7 +0,0 @@
-import { PageTitle } from "./styles";
-
-function Facebook() {
-  return <PageTitle>Welcome to the Facebook company page</PageTitle>;
-}
-
-export default Facebook;
--- src/components/pages/Clients/components/facebook/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const PageTitle = styled.div`
- font-size: 36px;
- font-weight: bold;
-`
