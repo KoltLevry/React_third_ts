f5d6cbf7c59d50f64731c35e75ea0012ab24b15d
Tanya <kolt.levry@gmail.com>
2025-02-06 20:16:03 +0100

    add new homework 14

---
+++ src/components/pages/homeworks/Homework14/Message/styles.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+export const MessageContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 30px;
+gap: 20px;
+width: 300px;
+height: 100px;
+background-color: #e354d0;
+`
+export const MessageComponent = styled.div`
+display: flex;
+flex-direction: column;
+`
